c77eefe0c723e9584b20a5b75c6650722185cfc5
Salubba <78054807+Salubba@users.noreply.github.com>
2021-02-01 13:35:17 +0100

    carrousel check circle

--- js/main.js
+++ js/main.js
@@ -2,12 +2,23 @@
 const objetoImg = document.querySelector('#imagen');
 const botonAvanzar = document.querySelector('#avanzar');
 const botonRetroceder = document.querySelector('#retroceder');
+//capturamos la template y su primer elemento
+const templateCirculo = document.querySelector('#template-circulo').content.firstElementChild;
+const circulos = document.querySelector('#circulos');
 const imagenes = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg'];
 // pagina para indicar donde estoy
 let pagina = 1;
 
 
 //FUNCIONES
+
+function cambiarPagina(nuevaPagina) {
+    pagina = nuevaPagina;
+    render();
+}
+
+
+
 function avanzarFoto() {
     pagina = pagina + 1;
     //Verificamos que no hemos alcanzado el limite, en caso contrario lo restauramos a 1
@@ -19,21 +30,39 @@ function avanzarFoto() {
 
 function retrocederFoto() {
     pagina = pagina - 1;
+    //comprobamos que no ha alcanzado el limite 0, en caso contrario lo restauramos a la ultima
     if (0 === pagina) {
-        pagina = imagenes.lenght;
+        pagina = imagenes.length;
     }
     render();
 }
 
 function render() {
-
-    objetoImg.setAttribute('src', imagenes[pagina - 1] )
-
+    //Imagen
+    objetoImg.setAttribute('src', imagenes[pagina - 1]);
+    //Circulos
+    circulos.textContent = '';
+    imagenes.forEach(function (imagen, indice) {
+        const nuevoCirculo = templateCirculo.cloneNode(true);
+        nuevoCirculo.addEventListener('click', function () {
+            cambiarPagina(indice + 1);
+        });
+        //Marcamos el que coincide con la pagina
+        if (pagina === indice + 1) {
+            nuevoCirculo.setAttribute('checked', true);
+        }
+        //Mostramos
+        circulos.appendChild(nuevoCirculo);
+    });
 }
+
+
+
 
 //EVENTOS
 botonAvanzar.addEventListener('click', avanzarFoto);
 botonRetroceder.addEventListener('click', retrocederFoto);
+
 
 
 //INICIO
